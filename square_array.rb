3bf1fd86fd9f23da2d307c74f42ed4ec10ef9f79
Keith Burgie <krburgie@gmail.com>
2019-04-25 21:12:22 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,15 @@
 numbers = [2, 4, 6]
 
 def square_array(numbers)
+  
   new_numbers = []
   numbers.each do |element|
-    new_numbers << element ** 2
+   new_numbers << element ** 2
   end
   new_numbers
+  
+  #numbers.collect { |number| number ** 2 }
+  
 end
-
 
 square_array(numbers)
